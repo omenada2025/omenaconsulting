29411aead1665613263781fe15a5ded5129d165c
omenada2025 <rdaniglad@hotmail.com>
2026-05-26 19:38:58 -0400

    Add Supabase config

---
+++ supabase-config.js
@@ -0,0 +1,5 @@
+window.SUPABASE_CONFIG = {
+  url: "https://qjxdfqtzcrsopprcmicr.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqeGRmcXR6Y3Jzb3BwcmNtaWNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4MDQwOTAsImV4cCI6MjA5NTM4MDA5MH0.svqbzxqQLkpt3OlHQdeHJlzRpvh-gzqk9PlzBbD5UNw",
+  table: "status_reports"
+};
