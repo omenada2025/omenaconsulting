2c3fa837abdbb1c9b65bedcead193bf6572a052f
omenada2025 <216609864+omenada2025@users.noreply.github.com>
2026-06-09 17:21:54 -0400

    Use deployed Supabase email function

--- supabase-config.js
+++ supabase-config.js
@@ -2,5 +2,7 @@ window.SUPABASE_CONFIG = {
   url: "https://qjxdfqtzcrsopprcmicr.supabase.co",
   anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqeGRmcXR6Y3Jzb3BwcmNtaWNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4MDQwOTAsImV4cCI6MjA5NTM4MDA5MH0.svqbzxqQLkpt3OlHQdeHJlzRpvh-gzqk9PlzBbD5UNw",
   table: "status_reports",
-  emailFunction: "send-credentials"
+  emailFunction: "send-credentials",
+  emailFunctionUrl: "https://vwvmfuktrkpzrzlklbkr.supabase.co/functions/v1/send-credentials",
+  emailFunctionAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ3dm1mdWt0cmtwenJ6bGtsYmtyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwMDUxMzcsImV4cCI6MjA5NTU4MTEzN30.oKJPiyz0Qv7ISIBMbAr7NaipufQ4ITRH1ArPIv4Szvk"
 };
